c00c1d1b90f80095b7b952daf12c6c94cba0d243
djkeu <mrkooij@gmail.com>
2021-03-01 21:04:04 +0100

    Guess the number (user)

--- guess_number/main.py
+++ guess_number/main.py
@@ -21,5 +21,26 @@ def guess(x):
     # Print doesn't have to be in the loop.
     print(f"You guessed right, {random_number} is the right number!")
 
-guess(10)
- 
+# guess(10)  # Uncomment to guess the random number again.
+
+
+def computer_guess(x):
+    """Have the computer guess the secret number."""
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':  # 'c' for correct
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # Could also be high, low == high
+
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)??").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"\nThe computer guessed your secret number, it's {guess}!") 
+
+computer_guess(10000)
