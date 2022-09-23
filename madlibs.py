86799ce4c88f1ec7e33d60a342ed0f109cf9291c
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-09-23 08:49:55 -0700

    Update madlibs.py
    Add replay madlib again

--- madlibs.py
+++ madlibs.py
@@ -19,12 +19,21 @@ Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5
 # print("subscribe to {}".format(youtuber))
 # print(f"subscribe to {youtuber}")
 
-adj = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous person: ")
+def word():
+    adj = input("Adjective: ")
+    verb1 = input("Verb: ")
+    verb2 = input("Verb: ")
+    famous_person = input("Famous person: ")
 
-madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+    madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+        I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
-print(madlib)
+    print(madlib)
+    #made replay game again function
+    question = input("Still wanna play ? (Y/N) ")
+    if question == 'Y' or question == 'y':
+        word()
+    else:
+        quit()
+
+word()
