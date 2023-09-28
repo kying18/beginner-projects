a9f6153e64af1b71fbffabb00b5dc3c5f8dccc7f
Umair khan <89532763+umairrrkhan@users.noreply.github.com>
2023-09-28 22:36:29 +0530

    added some error handling
    add some error handling in this file

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,20 +14,21 @@ import random
 
 def play():
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+
+    if user not in ['r', 'p', 's']:
+        return 'Invalid input. Please choose "r" for rock, "p" for paper, or "s" for scissors.'
+
     computer = random.choice(['r', 'p', 's'])
 
     if user == computer:
-        return 'It\'s a tie'
+        return "It's a tie"
 
-    # r > s, s > p, p > r
     if is_win(user, computer):
         return 'You won!'
 
     return 'You lost!'
 
 def is_win(player, opponent):
-    # return true if player wins
-    # r > s, s > p, p > r
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
         or (player == 'p' and opponent == 'r'):
         return True
